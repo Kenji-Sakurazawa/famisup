ca0d5e35f13802dbac4f46c5610983eabcee4b32
Kenji Sakurazawa <sakurajuku.mamapapa@gmail.com>
2019-09-20 22:48:41 +0900

    管理者ページ作成

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'rails_admin'
-gem 'device'
+gem 'devise'
 gem 'cancancan'
 
 
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { admin: 0, students: 1, mothers: 2, fathers: 3, others: 4 }
+end
